40aa16883b49cee83e6544366d7ecf710e4d3588
tukuro0101 <tutranz0101@gmail.com>
2024-06-11 21:06:36 -0500

    Complete Product model, controller, and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Commerce.price(range: 0..100.0),
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
+end
